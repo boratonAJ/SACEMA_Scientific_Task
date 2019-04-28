a1a6e620cb42d9df57cfce8b0efbf27058a4a73b
boratonaj <boraton2010@gmail.com>
2019-04-28 21:09:36 +0200

    script to compute the frequency of the words from an input file

--- script/wordfreq.py
+++ script/wordfreq.py
@@ -5,7 +5,9 @@ Program Version: 1.0
 Ajayi Olabode
 April 25, 2019
 
-This script count the words in the files and write their frequency in their respective files
+A script program to compute the frequency of the words from the input. The output is after sorting the key alphanumerically.
+Also note that this script count the words in the files and write their frequency in their respective files
+
 """
 
 from sys import argv, exit
